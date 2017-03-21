f6f3e9c5b8d230a958ce1286fb01e436fa9ac52b
agustin13 <agustin.rios136@gmail.com>
2017-03-21 13:58:10 -0700

    first commit

---
+++ jetspace-javascript/index.js
@@ -0,0 +1,12 @@
+function light(sw) {
+    var pic;
+    if (sw == 0) {
+        pic = "https://media.giphy.com/media/vrKUtJNMtB3Y4/giphy.gif"
+    } else {
+        pic = "https://s-media-cache-ak0.pinimg.com/originals/3a/88/ce/3a88ce0038c1b6da069e0332a44dcd08.gif"
+
+
+    }
+
+    document.getElementById('myImage').src = pic;
+}
